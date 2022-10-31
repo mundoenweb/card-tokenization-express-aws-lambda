12be368405463f51ef5a7b91d28fa22397e235bd
rommer <rommer@tuemprende.com>
2022-10-31 09:25:52 -0500

    se agrega middleware pra verificar token y se cambiam el nombre de la ruta token

--- src/components/card/cardRouter.ts
+++ src/components/card/cardRouter.ts
@@ -1,9 +1,10 @@
 import express from 'express'
+import { verifyTokenPay } from '../../middleware/verifyTokenPay'
 import * as controller from './cardController'
 
 const router = express.Router()
 
-router.post('/token', controller.createToken)
-router.get('/card', controller.createToken)
+router.get('/card', verifyTokenPay, controller.getCard)
+router.post('/card/token', controller.createToken)
 
 export default router
