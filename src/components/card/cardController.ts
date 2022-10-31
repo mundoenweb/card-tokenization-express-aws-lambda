e9e89f09763a3e22d0bcacfda4aa8fd6ba262df7
rommer <rommer@tuemprende.com>
2022-10-31 02:41:09 -0500

    se crean las funciones de los controladores a utilizar

---
+++ src/components/card/cardController.ts
@@ -0,0 +1,17 @@
+import {
+//   NextFunction as Next,
+  Request as Req,
+  Response as Res
+} from 'express'
+
+const createToken = (_req: Req, _res: Res): null => {
+  return null
+}
+const getCard = (_req: Req, _res: Res): null => {
+  return null
+}
+
+export {
+  createToken,
+  getCard
+}
