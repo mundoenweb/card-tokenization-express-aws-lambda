b6d0ad0098ee6ff46d39101f2b1b7ca4f8d2ff09
rommer <rommer@tuemprende.com>
2022-10-31 07:44:06 -0500

    se consfigura el controlador para: guardar los datos y generar el token

--- src/components/card/cardController.ts
+++ src/components/card/cardController.ts
@@ -3,13 +3,40 @@ import {
   Request as Req,
   Response as Res
 } from 'express'
+import { genereteToken } from './utils/genereteToken'
 import { validateData } from './utils/validateData'
+import jwt from 'jsonwebtoken'
+import createHttpError from 'http-errors'
+import * as models from './cardModel'
+import { helperResponse } from '../../utils/helperResponse'
+
+const SECRET_PASSWORD = 'secretPass.3467'
 
 const createToken = (req: Req, res: Res, next: Next): void => {
   const card = validateData(req.body, req, next)
   if (card === null) return next()
 
-  res.status(200).json({ token: 'ds5fs5df4', card })
+  const token: string = genereteToken(16)
+  const config = { expiresIn: 900 }
+
+  jwt.sign({ card }, SECRET_PASSWORD, config, (err, result) => {
+    if (err !== null) {
+      const r: any = req
+      r.error = createHttpError(500, err.message)
+      return next()
+    }
+
+    models.modelCreateToken(token, result)
+      .then(() => {
+        const message: string = 'Token creado exitosamente'
+        helperResponse(res, { token }, 201, message)
+      })
+      .catch((err: any) => {
+        const r: any = req
+        r.error = createHttpError(500, err.message)
+        return next()
+      })
+  })
 }
 const getCard = (_req: Req, _res: Res): null => {
   return null
