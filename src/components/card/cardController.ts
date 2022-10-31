0303f19390e0be936e9df937daf399577e9e244d
rommer <rommer@tuemprende.com>
2022-10-31 04:53:59 -0500

    se ejecuta validador de la data para probar y se manda respuesta ficticia

--- src/components/card/cardController.ts
+++ src/components/card/cardController.ts
@@ -1,11 +1,15 @@
 import {
-//   NextFunction as Next,
+  NextFunction as Next,
   Request as Req,
   Response as Res
 } from 'express'
+import { validateData } from './utils/validateData'
 
-const createToken = (_req: Req, _res: Res): null => {
-  return null
+const createToken = (req: Req, res: Res, next: Next): void => {
+  const card = validateData(req.body, req, next)
+  if (card === null) return next()
+
+  res.status(200).json({ token: 'ds5fs5df4', card })
 }
 const getCard = (_req: Req, _res: Res): null => {
   return null
