ad11bda9fd42cdf58534a09d8460249a5d6aa5d6
rommer <rommer@tuemprende.com>
2022-11-07 07:12:41 -0500

    se implemente middleware para verificar la data

--- src/components/card/cardRouter.ts
+++ src/components/card/cardRouter.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 import { verifyTokenPay } from '../../middleware/verifyTokenPay'
 import * as controller from './cardController'
+import { verifyDataCard } from './middleware/verifiDataCard/verifyDataCard'
 
 const router = express.Router()
 
-router.post('/', controller.createToken)
+router.post('/', verifyDataCard, controller.createToken)
 router.get('/', verifyTokenPay, controller.getCard)
 
 export default router
