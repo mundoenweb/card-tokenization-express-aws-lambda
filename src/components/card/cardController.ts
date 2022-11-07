b49e82f3a5a9bf242badd7a92db91af2d2c2c278
rommer <rommer@tuemprende.com>
2022-11-07 10:09:20 -0500

    se elimina redundancia de manejo de errores

--- src/components/card/cardController.ts
+++ src/components/card/cardController.ts
@@ -3,33 +3,28 @@ import {
   Request as Req,
   Response as Res
 } from 'express'
-import createHttpError from 'http-errors'
+// import createHttpError from 'http-errors'
 import { helperResponse } from '../../utils/helperResponse'
 import { newCard } from './types/interface'
 import { createTokenAndSaveCard, getCardService } from './service/cardService'
 
-const createToken = async (req: Req, res: Res, next: Next): Promise<void> => {
+export const createToken = async (req: Req, res: Res, next: Next): Promise<void> => {
   try {
     const card: newCard = req.body
     const token = await createTokenAndSaveCard(card)
-    helperResponse(res, token, 200)
+    helperResponse(res, token, 201, 'Token creado exitosamente')
   } catch (error: any) {
-    req.body.error = createHttpError(500, error.message)
+    req.body.error = error
     next('route')
   }
 }
-const getCard = async (req: Req, res: Res, next: Next): Promise<void> => {
+export const getCard = async (req: Req, res: Res, next: Next): Promise<void> => {
   try {
     const { token } = req.body // token obtenidos desde el middleware
     const card = await getCardService(token)
     helperResponse(res, card, 200, 'consulta exitosa')
   } catch (error: any) {
-    req.body.error = createHttpError(500, error.message)
+    req.body.error = error
     next('route')
   }
-}
-
-export {
-  createToken,
-  getCard
 }
