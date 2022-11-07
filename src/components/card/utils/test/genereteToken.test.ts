3b972225acd927f2e321b8c2ae38d48379065921
rommer <rommer@tuemprende.com>
2022-11-07 10:12:30 -0500

    se eliminan tes unitarios

--- src/components/card/utils/test/genereteToken.test.ts
+++
@@ -1,6 +0,0 @@
-import { genereteToken } from '../genereteToken'
-
-test(' si le pasamos uno la media seria 1', () => {
-  const token: string = genereteToken()
-  expect(token).toHaveLength(24)
-})
