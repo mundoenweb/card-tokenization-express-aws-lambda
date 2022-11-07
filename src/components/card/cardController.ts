8382b76a8beed1584c25f9d09dc7aedb6d2f20d9
rommer <rommer@tuemprende.com>
2022-11-07 08:11:54 -0500

    se muda funcionalida de getCard a getCardService

--- src/components/card/cardController.ts
+++ src/components/card/cardController.ts
@@ -3,66 +3,30 @@ import {
   Request as Req,
   Response as Res
 } from 'express'
-import { genereteToken } from './utils/genereteToken'
-import jwt from 'jsonwebtoken'
 import createHttpError from 'http-errors'
-import * as models from './cardModel'
 import { helperResponse } from '../../utils/helperResponse'
 import { newCard } from './types/interface'
+import { createTokenAndSaveCard, getCardService } from './service/cardService'
 
-const SECRET_PASSWORD = 'secretPass.3467'
-
-const createToken = (req: Req, res: Res, next: Next): void => {
-  const card: newCard = req.body
-  const token: string = genereteToken()
-  const config = { expiresIn: 900 }
-
-  jwt.sign({ card }, SECRET_PASSWORD, config, (err, result): void => {
-    if (err !== null) {
-      req.body.error = createHttpError(500, err.message)
-      return next()
-    }
-
-    models.modelCreateToken(token, result)
-      .then(() => {
-        const message: string = 'Token creado exitosamente'
-        helperResponse(res, { token }, 201, message)
-      })
-      .catch((err: any) => {
-        req.body.error = createHttpError(500, err.message)
-        next()
-      })
-  })
+const createToken = async (req: Req, res: Res, next: Next): Promise<void> => {
+  try {
+    const card: newCard = req.body
+    const token = await createTokenAndSaveCard(card)
+    helperResponse(res, token, 200)
+  } catch (error: any) {
+    req.body.error = createHttpError(500, error.message)
+    next('route')
+  }
 }
-const getCard = (req: Req, res: Res, next: Next): void => {
-  const { token } = req.body // token obtenidos desde el middleware
-  const msgExpOrNotExist: string = 'La información que busca no existe o a expirado, favor vuelva a intentar con nuevos datos'
-
-  models.modelGetCard(token)
-    .then((response: any[]) => {
-      if (response.length === 0) {
-        throw new Error(msgExpOrNotExist)
-      }
-
-      const cardToken: string = response[0].cardToken
-      jwt.verify(cardToken, SECRET_PASSWORD, (err: any, result: any): void => {
-        if (err !== null) {
-          if (err.message === 'jwt expired') {
-            models.deleteCard(token).then(() => { }).catch(() => { })
-            throw new Error(msgExpOrNotExist)
-          }
-          throw new Error(err.message)
-        }
-
-        const data = { ...result.card }
-        delete data.cvv
-        helperResponse(res, data, 200, 'consulta exitosa')
-      })
-    })
-    .catch(err => {
-      req.body.error = createHttpError(400, err.message)
-      next()
-    })
+const getCard = async (req: Req, res: Res, next: Next): Promise<void> => {
+  try {
+    const { token } = req.body // token obtenidos desde el middleware
+    const card = await getCardService(token)
+    helperResponse(res, card, 200, 'consulta exitosa')
+  } catch (error: any) {
+    req.body.error = createHttpError(500, error.message)
+    next('route')
+  }
 }
 
 export {
