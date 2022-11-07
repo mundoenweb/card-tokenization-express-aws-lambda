ce1335ea0a9c9596f7241db69eea46663ec7d3a6
rommer <rommer@tuemprende.com>
2022-11-07 07:43:43 -0500

    se importa e implementa dotenv y se agrega las variables de entorno

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,14 @@
-const mongoDB = {
-  host: 'mongodb+srv://',
-  cluster: '@cluster0.llztguf.mongodb.net/',
-  user: 'tuemprende',
-  password: 'adoracionviva.3467'
+import { config as configDotenv } from 'dotenv'
+configDotenv()
+
+const config = {
+  mongoDB: {
+    host: process.env.HOST,
+    cluster: process.env.CLUSTER,
+    user: process.env.USER,
+    password: process.env.DBPASSWORD
+  },
+  secretPassJWT: process.env.SECRET_PASSWORD_JWT
 }
 
-export default mongoDB
+export default config
