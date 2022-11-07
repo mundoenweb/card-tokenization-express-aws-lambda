94599e2035c4035fb2752b4e0b4eb9c5bc6eb905
rommer <rommer@tuemprende.com>
2022-11-07 07:22:38 -0500

    se refactoriza corrigiendo posibles bugs

--- src/middleware/verifyTokenPay.ts
+++ src/middleware/verifyTokenPay.ts
@@ -1,40 +1,30 @@
+import createError from 'http-errors'
 import {
   NextFunction as Next,
   Request as Req,
   Response as Res
 } from 'express'
-import createHttpError from 'http-errors'
 
 export const verifyTokenPay = (req: Req, _res: Res, next: Next): void => {
   const authorizationHeader: string | undefined = req.headers.authorization
+  const error: string = 'Acceso no autorizado'
+  const expToken: RegExp = /^[A-Za-z0-9]+$/
 
   if (authorizationHeader === undefined) {
-    req.body.error = createHttpError(403)
+    req.body.error = createError.Unauthorized(error)
     return next('route')
   }
 
-  const tkn: string = authorizationHeader.split(' ')[1]
-  const splitToken: string[] = tkn.split('_')
-  const token = splitToken[2]
+  const token: string = authorizationHeader.split(' ')[1]
 
-  if (splitToken[0] !== 'pk' || splitToken[1] !== 'test') {
-    req.body.error = createHttpError(401, 'token invalido')
+  if (token.length !== 16 || !expToken.test(token)) {
+    req.body.error = createError.Unauthorized(error)
     return next('route')
   }
 
-  if (token.length !== 16) {
-    req.body.error = createHttpError(401, 'token invalido')
-    return next('route')
-  }
-
-  const characters: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-  for (const chart of token) {
-    if (!characters.includes(chart)) {
-      req.body.error = createHttpError(401, 'token invalido')
-      return next('route')
-    }
-  }
-
-  req.body.token = tkn
+  req.body.token = token
   next()
 }
+
+// req.body.error = createHttpError(401, 'token invalido')
+// return next('route')
